fdd18ebd5bd8cbb6ca2c49732edab69a7b886c1f
Daniel Modrzejewski <danio1118m@gmail.com>
2019-06-05 08:16:58 +0200

    refactor - add ChuckConfiguration (@Bean annotation)

---
+++ src/main/java/daniel/modrzejewski/joke/jokeapp/configuration/ChuckConfiguration.java
@@ -0,0 +1,14 @@
+package daniel.modrzejewski.joke.jokeapp.configuration;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
