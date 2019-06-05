a07360374fdbe1901a145a55b2d1878b130e3910
Daniel Modrzejewski <danio1118m@gmail.com>
2019-06-05 08:30:00 +0200

    test xml Spring configuration

--- src/main/java/daniel/modrzejewski/joke/jokeapp/JokeappApplication.java
+++ src/main/java/daniel/modrzejewski/joke/jokeapp/JokeappApplication.java
@@ -2,8 +2,10 @@ package daniel.modrzejewski.joke.jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-conf.xml")
 public class JokeappApplication {
 
     public static void main(String[] args) {
